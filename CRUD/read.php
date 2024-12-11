1a86de7dad16b0dafc360f17234be9d2dd4ae9a5
saviour000 <kachak331@gmail.com>
2024-12-11 17:00:49 +0530

    Update read.php

--- CRUD/read.php
+++ CRUD/read.php
@@ -2,41 +2,47 @@
 include 'db_conn.php';
 
 $query = "SELECT * FROM FORM";
-$data = mysqli_query($conn,$query);
+$data = mysqli_query($conn, $query);
 
 $total = mysqli_num_rows($data);
 
-if($total != 0)
-{
+
+
+if ($total != 0) {
     ?>
-<h1 >Display All Records</h1>
-<a href="form.php">Add New Data</a>
+    <h1>Display All Records</h1>
+    <a href="form.php">Add New Data</a>
 
     <table border="2px">
         <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Gender</th>
-        <th>Address</th>
-        <th>Opertion</th>
-        
-        
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Address</th>
+            <th>Opertion</th>
+
+
         </tr>
 
-    <?php
-    while($result = mysqli_fetch_assoc($data))
-    {
-        echo " <tr>
-        <td>".$result['id']."</td>
-        <td>".$result['fname']."</td>
-        <td>".$result['lname']."</td>
-        <td>".$result['Gender']."</td>
-        <td>".$result['Address']."</td>
+
+
+        <?php
+        while ($result = mysqli_fetch_assoc($data)) {
+            echo " <tr>
+        <td>" . $result['id'] . "</td>
+        <td>" . $result['fname'] . "</td>
+        <td>" . $result['lname'] . "</td>
+        <td>" . $result['Gender'] . "</td>
+        <td>" . $result['Address'] . "</td>
         <td><a href='update.php?id=$result[id]'>Update</a>
         <a href='delete.php?id=$result[id]'>Delete</a></td>
         </tr>
         ";
-    }
+        }
 }
+
+
+
 ?>
+
 </table>
