33e5620520c3c4c7009c3aa14ad435324a38c6b2
saviour000 <kachak331@gmail.com>
2024-12-11 18:28:58 +0530

    Update db_conn.php

--- CRUD/db_conn.php
+++ CRUD/db_conn.php
@@ -2,6 +2,8 @@
 
 $conn = mysqli_connect('localhost', 'root', '', 'new');
 
+// new is Database Name , Change new and replace it with your Database Name
+
 if(!$conn){
     die("Not Connected" . mysqli_connect_error());
 }
